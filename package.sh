667dfd7f1f0dda7ed5d875e8a548b63e72386bbb
Francisco Silva <contato@franciscoanderson.com>
2024-07-08 19:56:49 -0300

    fix: ignoring non-relevant file in the package

--- package.sh
+++ package.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
 
 glib-compile-schemas ./schemas/
-zip -r 'respect-do-not-disturb@farodriguess.zip' . -x ".git/*"
+rm 'respect-do-not-disturb@farodriguess.zip'
+zip -r 'respect-do-not-disturb@farodriguess.zip' . -x ".git/*" -x ".gitignore" -x "install.sh" -x "package.sh" -x "README.md"
